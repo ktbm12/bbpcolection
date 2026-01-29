81b3ff3b7f8a8301f026628659e620d0b47fd784
ktbm <boliventtalla12@gmail.com>
2026-01-29 13:28:45 +0100

    update

--- bbpproject/product/urls.py
+++ bbpproject/product/urls.py
@@ -1,10 +1,11 @@
-# bbpproject/users/urls.py
-
+# product/urls.py
 from django.urls import path
- # ← une seule fois bbpproject
-app_name = "product"
+from .views.product_list_view import ProductListView
+
+app_name = 'product'
 
 urlpatterns = [
-   
-    
+    path('', ProductListView.as_view(), name='product_list'),
+    # path('<slug:slug>/', ProductDetailView.as_view(), name='product_detail'),
+    # etc.
 ]
