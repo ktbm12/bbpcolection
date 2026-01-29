efa576902d708b4e3b5e43780a3e7e4fa6ad3429
ktbm <boliventtalla12@gmail.com>
2026-01-29 10:59:59 +0100

    update template

--- bbpproject/bbpproject/users/urls.py
+++ bbpproject/bbpproject/users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from bbpproject.bbpproject.users.views.home_view import home_view
+
 from .views import user_detail_view
 from .views import user_redirect_view
 from .views import user_update_view
@@ -9,4 +11,5 @@ urlpatterns = [
     path("~redirect/", view=user_redirect_view, name="redirect"),
     path("~update/", view=user_update_view, name="update"),
     path("<str:username>/", view=user_detail_view, name="detail"),
+    path("", view=home_view, name="home"),
 ]
---
+++ bbpproject/bbpproject/users/views/home_view.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+def home_view(request):
+    return render(request, 'pages/home.html')
+
+from django.shortcuts import render
+
+def home_view(request):
+    return render(request, 'pages/home.html')
+
+from django.shortcuts import render
+
+def home_view(request):
+    return render(request, 'pages/home.html')
