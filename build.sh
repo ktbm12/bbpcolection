8f90bdb8cdf89e4f8854359d86a4e22d8ab2f0d5
ktbm <boliventtalla12@gmail.com>
2026-03-25 13:26:08 +0100

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Installer les dépendances
 pip install -r requirements.txt
+
+# Collecter les fichiers statiques
 python manage.py collectstatic --noinput
+
+# Appliquer les migrations
 python manage.py migrate
