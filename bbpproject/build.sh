e22ec86218111470007ff769c0a9bc362974fc0e
ktbm <boliventtalla12@gmail.com>
2026-03-25 13:48:14 +0100

    update

--- bbpproject/build.sh
+++ bbpproject/build.sh
@@ -4,8 +4,8 @@ set -o errexit
 # Installer les dépendances
 pip install -r requirements.txt
 
-# Collecter les fichiers statiques
-python manage.py collectstatic --noinput
-
 # Appliquer les migrations
 python manage.py migrate
+
+# Collecter les fichiers statiques
+python manage.py collectstatic --noinput
