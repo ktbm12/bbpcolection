89484dd7113a4be2df9ffeb7d64a909fd25581d3
ktbm <boliventtalla12@gmail.com>
2026-03-26 10:23:19 +0100

    Add empty migration for core app to sync model state

---
+++ bbpproject/core/migrations/0005_auto_20260326.py
@@ -0,0 +1,13 @@
+# Generated manually on 2026-03-26
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0004_delete_privacypolicy'),
+    ]
+
+    operations = [
+    ]
