9ae3b7310f8d4006b3a7a7b1d9ec98f4c053c2f3
ktbm <boliventtalla12@gmail.com>
2026-03-26 09:20:12 +0100

    upfaty

--- bbpproject/config/settings/production.py
+++ bbpproject/config/settings/production.py
@@ -156,7 +156,7 @@ LOGGING = {
 
 # Sentry
 # ------------------------------------------------------------------------------
-SENTRY_DSN = env("SENTRY_DSN")
+
 SENTRY_LOG_LEVEL = env.int("DJANGO_SENTRY_LOG_LEVEL", logging.INFO)
 
 sentry_logging = LoggingIntegration(
