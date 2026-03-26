8dc822a2fca405d6ef01c586ac377d839db9fbe3
ktbm <boliventtalla12@gmail.com>
2026-03-26 04:09:13 +0100

    upfaty

--- bbpproject/config/settings/production.py
+++ bbpproject/config/settings/production.py
@@ -20,6 +20,7 @@ from .base import env
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["example.com"])
+ADMIN_URL = env("DJANGO_ADMIN_URL", default="admin/")
 
 # DATABASES
 # ------------------------------------------------------------------------------
